687526c311535651a3444879c72c2075d03485f2
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-30 23:38:30 -0400

    Remove more gitkeep

--- spikes/cli-app/tests/test-helper.ts
+++ spikes/cli-app/tests/test-helper.ts
@@ -1,9 +1,10 @@
+import { setApplication } from '@ember/test-helpers';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
+import { setupEmberOnerrorValidation, start as qunitStart } from 'ember-qunit';
+
 import Application from 'cli-app/app';
 import config from 'cli-app/config/environment';
-import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
 
 export function start() {
   setApplication(Application.create(config.APP));
