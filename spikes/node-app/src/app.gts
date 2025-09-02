8cffa63f351a846028f2423bdfc241943810f078
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-02 00:08:25 -0400

    omg

--- spikes/node-app/src/app.gts
+++ spikes/node-app/src/app.gts
@@ -1,26 +1,18 @@
-import './renderer.ts';
-
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { renderComponent } from '@ember/renderer';
 
-// import { Button, Text } from './terminal.ts';
+import { DraggableButton } from './terminal.ts';
 
 class HelloWorld extends Component {
   @tracked count = 0;
 
   increment = () => (this.count += 1);
 
-  get message() {
-    return `You have clicked the button ${this.count} times.`;
-  }
-
   <template>
-    You have clicked the button
-    {{this.count}}
-    times.
+    You have clicked {{this.count}} times.
 
-    {{! Button "Click" this.increment }}
+    {{DraggableButton "Click" this.increment}}
   </template>
 }
 
--- spikes/node-app/src/terminal.ts
+++ spikes/node-app/src/terminal.ts
@@ -1,15 +1,45 @@
 /* eslint-disable no-console */
-import Helper from '@ember/component/helper';
-
 import {
   Button as TerminalButton,
   ConsoleManager,
   PageBuilder,
 } from 'console-gui-tools';
+import { Window } from 'happy-dom';
+
+const universalUserAgent = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36`;
+
+const window = new Window({
+  console: globalThis.console,
+  settings: {
+    navigator: {
+      userAgent: universalUserAgent,
+    },
+  },
+});
+
+window.document.body.innerHTML = '<div></div>';
+
+const { navigator, crypto, ...toApply } = window;
+
+Object.assign(globalThis, toApply, {
+  window: window,
+});
+
+process.on('exit', () => {
+  window.close();
+});
 
 const p = new PageBuilder(5);
-
-const GUI = new ConsoleManager({ title: 'Ember in Node' });
+const GUI = new ConsoleManager({
+  title: 'Ember in Node',
+  enableMouse: true,
+  logLocation: 1,
+  layoutOptions: {
+    boxed: true,
+    type: 'single',
+    fitHeight: true,
+  },
+});
 
 GUI.setPage(p, 0);
 
@@ -18,66 +48,97 @@ GUI.on('exit', () => {
   process.exit();
 });
 
-let texts: string[] = [];
+let texts: TextNode[] = [];
+
+function trim(node: TextNode): string {
+  return node.textContent.replaceAll(/\s\s/g, '').trim();
+}
 
 function updatePage() {
   p.clear();
 
-  for (const text of texts) {
-    p.addRow({ text });
-  }
+  const text = texts.map(trim).join(' ');
 
-  // console.log(JSON.stringify(texts));
+  p.addRow({
+    text,
+    color: 'white',
+    bold: true,
+    hidden: false,
+  });
+  GUI.setPage(p);
   GUI.refresh();
 }
 
-export function addText(text: string) {
-  if (texts.includes(text)) {
-    removeText(text);
-  }
-
+export function addText(text: TextNode) {
   texts.push(text);
-
-  updatePage();
-}
-
-export function removeText(text: string) {
-  texts = texts.filter((t) => t === text);
 
   updatePage();
 }
 
 let id = 0;
 
-export class Text extends Helper {
-  // #id = `text_${id++}`;
-  compute([text]: [string, () => void]) {
-    p.addRow({ text });
-    GUI.refresh();
+export function DraggableButton(text: string, callback: () => void) {
+  const button = new TerminalButton({
+    text,
+    id: `button_${id++}`,
+    x: 20,
+    y: 10,
+    draggable: true,
+    style: {
+      color: 'magentaBright',
+      bold: true,
+      borderColor: 'cyan',
+    },
+  });
 
-    return '';
+  button.on('click', () => {
+    callback();
+  });
+  GUI.refresh();
+
+  return undefined;
+}
+
+let timer: number | undefined;
+
+function scheduleUpdate() {
+  clearTimeout(timer);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  timer = setTimeout(() => {
+    // const text = texts.map(trim).join(' ');
+
+    // console.log(`up(${texts.length}): ${text}`);
+    updatePage();
+  }, 50);
+}
+
+class TextNode extends window.Text {
+  constructor(text: string) {
+    super(text);
+
+    addText(this);
+  }
+
+  // data, textContent, nodeValue
+  get textContent() {
+    return super.textContent;
+  }
+  set textContent(value) {
+    // console.log(`Updating ${this.textContent} to ${value}`);
+    super.textContent = value;
+
+    scheduleUpdate();
+  }
+
+  remove() {
+    super.remove();
+
+    texts = texts.filter((x) => x === this);
   }
 }
 
-export class Button extends Helper {
-  #id = `button_${id++}`;
-  compute([text, callback]: [string, () => void]) {
-    const button = new TerminalButton({
-      text,
-      id: this.#id,
-      x: 0,
-      y: 0,
-      draggable: true,
-      style: {
-        color: 'magentaBright',
-        bold: true,
-        borderColor: 'cyan',
-      },
-    });
-
-    button.on('click', callback);
-    GUI.refresh();
-
-    return '';
-  }
-}
+window.document.createTextNode = (text) => {
+  return new TextNode(text);
+};
