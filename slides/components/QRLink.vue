0fa5b9ada47c27ae4bff198bcb988bc968d964c7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-08 21:37:59 -0400

    Progress

---
+++ slides/components/QRLink.vue
@@ -0,0 +1,19 @@
+<script setup>
+	import QRCode from 'slidev-addon-qrcode/components/QRCode.vue';
+
+	const props = defineProps(['href', 'label'])
+</script>
+
+
+<template>
+	<a href="{{ props.href }}">
+		<div class="top-right-link-image-overlay">
+			<QRCode
+				class="qr-code"
+				type="svg"
+				data="{{ props.href }}"
+			/>
+			<span class="text-black">{{ props.label ?? props.href }}</span>
+		</div>
+	</a>
+</template>
