36d4f22a01b234f2601d5c3b58fc52be4ca1a858
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-11 19:21:44 -0400

    Oh yeah

---
+++ slides/public/demos/effects/assets/main-B2CH9cfB.js
@@ -0,0 +1,4 @@
+import { A as App, E as ENV } from './app-CAlvou1D.js';
+
+App.create(ENV.APP);
+//# sourceMappingURL=main-B2CH9cfB.js.map
--- spikes/effects-app/vite.config.mjs
+++ spikes/effects-app/vite.config.mjs
@@ -3,6 +3,9 @@ import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [
     ember(),
     // extra plugins here
