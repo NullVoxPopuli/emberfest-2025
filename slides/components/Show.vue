3575c1af4415409e6e2a33c261af6a8bb981f10f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-15 12:19:13 -0400

    oof

---
+++ slides/components/Show.vue
@@ -0,0 +1,33 @@
+
+<script setup lang="ts">
+	/**
+	Sli.dev is inanely buggy around click visibility
+
+	https://github.com/slidevjs/slidev/issues/860
+	*/
+import {inject, ref, onMounted} from "vue";
+import { onSlideEnter, onSlideLeave, useDarkMode, useIsSlideActive, useNav, useSlideContext } from '@slidev/client'
+import { parseRangeString } from '@slidev/parser/core'
+
+const props = defineProps<{
+  on: string
+}>()
+
+const { $clicks, $frontmatter } = useSlideContext();
+
+const show = () => {
+	const totalClicks = { ...$frontmatter}.clicks;
+	let ranges = parseRangeString(totalClicks, props.on)
+	let found = ranges.indexOf($clicks.value);
+	// console.log({ $clicks, found, ranges, totalClicks, $frontmatter });
+
+	return found !== -1
+}
+
+</script>
+
+<template>
+  <div v-if="show()">
+    <slot/>
+  </div>
+</template>
